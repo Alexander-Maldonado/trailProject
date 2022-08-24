55b624ab584f65f9e05afd72fa2290929bf94111
Alexander-Maldonado <alex_ahc@yahoo.com>
2022-08-24 08:54:52 -0700

    user controller update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,41 +1,41 @@
-const user =  require("../models/user.model")
+const users =  require("../models/user.model")
 
 module.exports = {
 
-    getUser:(req,res)=>{
-        user.find({})
-        .then((users) =>{res.json(users)})
+    getUsers:(req,res)=>{
+        users.find({})
+        .then((allUsers) =>{res.json(allUsers)})
         .catch((err)=>{console.log("Error in finding all users")
         res.status(400).json({message:"Something went wrong in finding all users information", err})
     })
     },
     
-    createUser:(req,res)=>{
-        user.create(req.body)
-        .then((createUser)=>{res.status(201).json(createUser)})
+    createUsers:(req,res)=>{
+        users.create(req.body)
+        .then((createUsers)=>{res.status(201).json(createUsers)})
         .catch((err)=>{console.log("error in creating a user", err)
         res.status(400).json({message:"Something went wrong in creating user"})
     })
     },
     
     getUserById:(req,res)=>{
-        user.findOne({_id:req.params.id})
-        .then((users)=>{res.json(users)})
+        users.findOne({_id:req.params.id})
+        .then((user)=>{res.json(user)})
         .catch((err)=>{console.log("error in getting this id")
         res.status(400).json({message:"Something went wrong in finding this id"},err)
         })
     },
     
     updateUserById:(req,res)=>{
-        user.findOneAndUpdate({id:req.params.id}, req.body, {new:true, runValidators:true})
+        users.findOneAndUpdate({id:req.params.id}, req.body, {new:true, runValidators:true})
         .then((updateUser)=>res.json(updateUser))
         .catch((err)=>{console.log("error in updating values", err)
         })
     },
     
     deleteUser:(req,res)=>{
-        user.findByIdAndDelete({_id:req.params.id})
+        users.findByIdAndDelete({_id:req.params.id})
         .then((deleteUser)=>res.json(deleteUser))
         .catch((err)=>res.status(400).json(err));
-    }
+    },
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,8 +2,8 @@ const user = require("../controllers/user.controller")
 
 module.exports = (app) =>{
 
-    app.get("/api/trails/users", user.getUser)
-    app.post("/api/trails/user", user.createUser)
+    app.get("/api/trails/users", user.getUsers)
+    app.post("/api/trails/user", user.createUsers)
     app.get("/api/trails/user/:id", user.getUserById)
     app.put("/api/trails/user/:id", user.updateUserById)
     app.delete("/api/trails/user/:id", user.deleteUser)
